a3fb688678a5f63c762026fc4f580dc6e7808bf0
fumio-tiger <fumio.t@tiger-shokai.com>
2026-02-12 02:38:31 +0900

    サイトタイトルと説明を更新し、初めての投稿を追加しました。

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'No Man\'s Land';
+export const SITE_DESCRIPTION = 'インターネットの片隅でひっそりと暮らしたいです';
